0c52cfd5e6f385c9971f036832f3344ba739f0f4
abcd@1234 <chenzuo@hotmail.com>
2019-01-30 23:36:33 +0800

    add test funciont

--- dist/test/index.test.js
+++ dist/test/index.test.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../src/index");
 describe("Test index.ts/ hrx", () => {
     test("isArrayLike(): true", () => {
         expect(3).toBe(3);
+    });
+    test("add(): number", () => {
+        const result = index_1.add(1, 2);
+        expect(result).toBe(3);
     });
 });
 //# sourceMappingURL=index.test.js.map
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,4 +3,8 @@ describe("Test index.ts/ hrx", () => {
     test("isArrayLike(): true", () => {
         expect(3).toBe(3);
     });
+    test("add(): number", () => {
+        const result = add(1, 2);
+        expect(result).toBe(3);
+    });
 });
