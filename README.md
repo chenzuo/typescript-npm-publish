d52f1f34b081b75eb9fff9a0e60ca212ecc0030f
abcd@1234 <chenzuo@hotmail.com>
2019-01-30 13:52:53 +0800

    init npm pack,init typesciprt files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # typescript-npm-publish
+
+This is a test package for my NPM tutorial.
+
+## License
+
+This package is published under [MIT license](./LICENSE).
---
+++ test/index.test.ts
@@ -0,0 +1,6 @@
+import { add } from "../src/index";
+describe("Test index.ts/ hrx", () => {
+    test("isArrayLike(): true", () => {
+        expect(3).toBe(3);
+    });
+});
