ab03980ac4a1241dc5c9d9fbac58d92f7a4c0a73
abcd@1234 <chenzuo@hotmail.com>
2019-01-30 15:39:01 +0800

    add the directory of dist files

---
+++ dist/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 非常简单的加法函数;
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+//# sourceMappingURL=index.js.map
---
+++ dist/test/index.test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe("Test index.ts/ hrx", () => {
+    test("isArrayLike(): true", () => {
+        expect(3).toBe(3);
+    });
+});
+//# sourceMappingURL=index.test.js.map
